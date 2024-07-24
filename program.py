391e60eab9d53abb6dcff36d8305ac23cc7acc42
Anuj Sharma <anuj_sharma401@yahoo.com>
2024-07-24 17:20:21 +0530

    Fixed Program.py

---
+++ program.py
@@ -0,0 +1,13 @@
+def factorial(x):
+    """This is a recursive function
+    to find the factorial of an integer"""
+
+    if x == 1:
+        return 1
+    else:
+        print(x)
+        return (x * factorial(x-1))
+
+
+num = 3
+print("The factorial of", num, "is", factorial(num))
